2c206384b4e8eeb856ea74f31b23540c6a592830
NSPatton <greengun15@gmail.com>
2021-04-07 14:12:23 -0400

    added details

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { Home } from "./Home";
-import { AnimalCard } from "./animal/AnimalCard";
+import { AnimalList } from "./animal/AnimalList";
 import { LocationCard } from "./location/LocationCard";
 import { CustomerCard } from "./customer/CustomerCard";
 import { EmployeeCard } from "./employee/EmployeeCard";
+import { AnimalDetail } from "./animal/AnimalDetail";
+import { Link } from "react-router-dom";
 
 export const ApplicationViews = () => {
     return (
@@ -15,11 +17,23 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-                <AnimalCard />
-                <AnimalCard />
+            {/* Make sure you add the `exact` attribute here */}
+            <Route exact path="/animals">
+                <AnimalList />
             </Route>
+
+            <Route path="/animals/:animalId(\d+)">
+                <AnimalDetail />
+            </Route>
+
+            {/*
+  This is a new route to handle a URL with the following pattern:
+  http://localhost:3000/animals/1
+
+  It will not handle the following URL because the `(\d+)`
+  matches only numbers after the final slash in the URL
+  http://localhost:3000/animals/jack
+*/}
 
             <Route path="/locations">
                 <LocationCard />
---
+++ src/components/animal/AnimalDetail.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { getAnimalById } from "../../modules/AnimalManager";
+import "./AnimalDetail.css";
+import { useParams, useHistory, Link } from "react-router-dom"
+
+export const AnimalDetail = () => {
+    const [animal, setAnimal] = useState({ name: "", breed: "" });
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { animalId } = useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        //getAnimalById(id) from AnimalManager and hang on to the data; put it into state
+        console.log("useEffect", animalId)
+        getAnimalById(animalId)
+            .then(animal => {
+                setAnimal({
+                    name: animal.name,
+                    breed: animal.breed
+                });
+                setIsLoading(false);
+            });
+    }, [animalId]);
+
+
+    const handleDelete = () => {
+        setIsLoading(true);
+        delete (animalId).then(() =>
+            history.push("/animals")
+        );
+    };
+
+    return (
+        <section className="animal">
+            <h3 className="animal__name">{animal.name}</h3>
+            <div className="animal__breed">{animal.breed}</div>
+            {/* What's up with the question mark???? See below.*/}
+            <div className="animal__location">Location: {animal.location?.name}</div>
+            <div className="animal__owner">Customer: {animal.customer?.name}</div>
+            <Link to={`/animals/`}>
+                <button>Return</button>
+            </Link>
+            <button type="button" disabled={isLoading} onClick={handleDelete}>
+                Discharge
+        </button>
+        </section>
+    );
+};
